c23918eb8f33a6f324941f054156b0aec2234063
Настя <ablovatskaja@gmail.com>
2021-11-21 23:15:38 +0300

    delete img

--- src/Components/MainContent/Profile/ProfileInfo.tsx
+++ src/Components/MainContent/Profile/ProfileInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Profile.module.css';
+
 import {Preloader} from '../../Common/Preloader';
 import {ProfileType} from '../../../redux/profileReduser';
 import {ProfileStatus} from './ProfileStatus';
@@ -16,11 +16,7 @@ export let ProfileInfo = (props:PropsType) => {
         return <Preloader/>
     }
     return <div>
-        <div className={s.mainImg}>
-            <img
-                src={'https://images.pexels.com/photos/756861/pexels-photo-756861.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}
-                alt="Main Picture"/>
-        </div>
+
         <img src={props.profile.photos.large}/>
         <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
     </div>
